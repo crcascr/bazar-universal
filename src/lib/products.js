ea0b2cc24c714f1cf78a59a05b8599eac511bfd6
crcascr <cristianj_20@hotmail.com>
2024-06-27 15:30:35 -0500

    Correccion respuesta getProducts y cambio direccion thumbnail

--- src/lib/products.js
+++ src/lib/products.js
@@ -4,5 +4,6 @@ import path from "path";
 export async function getProducts() {
   const filePath = path.join(process.cwd(), "public", "products.json");
   const jsonData = await fs.readFile(filePath, "utf-8");
-  return JSON.parse(jsonData);
+  const data= JSON.parse(jsonData);
+  return data.products
 }
